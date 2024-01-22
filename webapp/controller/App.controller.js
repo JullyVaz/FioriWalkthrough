e0a0fabf0bb27f28b7d597a83d38313f629ee7e7
Juliane Oliveira <oliveirajulianer@johndeere.com>
2024-01-22 15:38:34 -0300

    Step 36

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
- ], (Controller) => {
+], (Controller) => {
 	"use strict";
- 
+
 	return Controller.extend("ui5.walkthrough.controller.App", {
+
+		onInit() {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
 	});
- });
+});
